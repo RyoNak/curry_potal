a168a856fe1b6fc3496c7e51e48dc7b7787647bd
nakataniaq <ryo.nakatani@asia-quest.jp>
2023-01-07 12:14:30 +0900

    initial commit

---
+++ src/resources/views/components/sidebar.blade.php
@@ -0,0 +1,9 @@
+<div class="sidebar shadow1">
+    <ul class="sidebar-menu sip">
+        <li><span><i class="fa-solid fa-chart-line faw"></i></span>顧客分析</li>
+        <li><span><i class="fa-solid fa-bookmark faw"></i></span>予約管理</li>
+        <li><span><i class="fa-solid fa-utensils faw"></i></span>コース管理</li>
+        <li><span><i class="fa-solid fa-wine-glass faw"></i></span>メニュー管理</li>
+        <li><span><i class="fa-solid fa-store faw"></i></span>店舗情報</li>
+    </ul>
+</div>
